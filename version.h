cfefa3b98a70a915d322e341ba8e4f054856e6df
lapo <lapo.chiostrini1@stud.unifi.it>
2022-04-06 15:49:37 +0100

    aggiunto resize

---
+++ version.h
@@ -0,0 +1,13 @@
+//
+// Created by lapo on 06/04/22.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+
+static const std::string VERSION = "V.1.0";
